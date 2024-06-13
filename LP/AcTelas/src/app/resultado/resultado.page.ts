98d4d36cb4180a763110508fb36a7128f2aaccfe
LuCheMaru <lucca07@outlook.com>
2024-06-13 10:41:24 -0300

    AcTelas

--- LP/AcTelas/src/app/resultado/resultado.page.ts
+++ LP/AcTelas/src/app/resultado/resultado.page.ts
@@ -16,6 +16,14 @@ export class ResultadoPage implements OnInit {
     medTemp: any|undefined;
     milho = "";
     cenoura = "";
+    melancia = "";
+    cebola = "";
+    vagem = "";
+    arroz = "";
+    gengibre = "";
+    lixia = "";
+    beterraba = "";
+    alcachofra = "";
 
   ngOnInit() {
     this.medTemp = this.ar.snapshot.paramMap.get('medTemp');
@@ -24,6 +32,30 @@ export class ResultadoPage implements OnInit {
     }
     if(this.medTemp >= 18 && this.medTemp <= 25){
       this.cenoura = "cenoura - 18ºC-25ºC"
+    }
+    if(this.medTemp >= 20 && this.medTemp <= 34){
+      this.melancia = "melancia - 20ºC-34ºC"
+    }
+    if(this.medTemp >= 1 && this.medTemp <= 24){
+      this.cebola = "cebola - 1ºC-24ºC"
+    }
+    if(this.medTemp >= 21 && this.medTemp <= 25){
+      this.vagem = "vagem - 21ºC-25ºC"
+    }
+    if(this.medTemp >= 20 && this.medTemp <= 35){
+      this.arroz = "arroz - 20ºC-35ºC"
+    }
+    if(this.medTemp >= 17 && this.medTemp <= 35){
+      this.gengibre = "gengibre - 17ºC-35ºC"
+    }
+    if(this.medTemp >= 20 && this.medTemp <= 35){
+      this.lixia = "lixia - 20ºC-35ºC"
+    }
+    if(this.medTemp >= 10 && this.medTemp <= 20){
+      this.beterraba = "beterraba - 10ºC-20ºC"
+    }
+    if(this.medTemp >= 12 && this.medTemp <= 22){
+      this.alcachofra = "alcachofra - 12ºC-22ºC"
     }
   }
 
