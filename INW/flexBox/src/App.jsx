280b2b9359e9e95b535823a2e606e02287aad44a
LuCheMaru <lucca07@outlook.com>
2024-09-04 12:22:08 -0300

    flexBox

---
+++ INW/flexBox/src/App.jsx
@@ -0,0 +1,140 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <div className='caixa0'></div>
+      </header>
+      <body>
+        <div className='retangulo'>
+          <div className='caixa1'>
+          </div>
+          <div className='caixa1'>
+          </div>
+          <div className='caixa1'>
+          </div>
+        </div>
+        <div className='caixa10'>
+          <div className='caixa'>
+            <div className='caixa1'>
+              <div className='caixa2'>
+              
+              </div>
+              <div className='caixa2'>
+              
+              </div>
+            </div>
+
+            <div className="caixa1">
+              <div className='caixa2'>
+              
+              </div>
+              <div className='caixa2'>
+              
+              </div>
+            </div>
+
+            <div className="caixa1">
+              <div className='caixa2'>
+              
+              </div>
+              <div className='caixa2'>
+              
+              </div>
+            </div>
+
+            <div className="caixa1">
+              <div className='caixa2'>
+              
+              </div>
+              <div className='caixa2'>
+              
+              </div>
+            </div>
+          </div>
+
+          <div className='caixa'>
+            <div className='caixa1'>
+              <div className='caixa2'>
+              
+              </div>
+              <div className='caixa2'>
+              
+              </div>
+            </div>
+
+            <div className="caixa1">
+              <div className='caixa2'>
+              
+              </div>
+              <div className='caixa2'>
+              
+              </div>
+            </div>
+
+            <div className="caixa1">
+              <div className='caixa2'>
+              
+              </div>
+              <div className='caixa2'>
+              
+              </div>
+            </div>
+
+            <div className="caixa1">
+              <div className='caixa2'>
+              
+              </div>
+              <div className='caixa2'>
+              
+              </div>
+            </div>
+          </div>
+
+          <div className='caixa'>
+            <div className='caixa1'>
+              <div className='caixa2'>
+              
+              </div>
+              <div className='caixa2'>
+              
+              </div>
+            </div>
+
+            <div className="caixa1">
+              <div className='caixa2'>
+              
+              </div>
+              <div className='caixa2'>
+              
+              </div>
+            </div>
+
+            <div className="caixa1">
+              <div className='caixa2'>
+              
+              </div>
+              <div className='caixa2'>
+              
+              </div>
+            </div>
+
+            <div className="caixa1">
+              <div className='caixa2'>
+              
+              </div>
+              <div className='caixa2'>
+              
+              </div>
+            </div>
+          </div>
+        </div>
+      </body>
+    </>
+  )
+}
+
+export default App
