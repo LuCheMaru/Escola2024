c9c21e66d75fd1539e7ddbc6429abf35bc184b76
LuCheMaru <lucca07@outlook.com>
2024-10-16 12:18:40 -0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- INW/flexBox/src/App.jsx
+++ INW/flexBox/src/App.jsx
@@ -1,4 +1,3 @@
-
 import { useState } from 'react'
 import CardSidebar from './components/CardSidebar'
 import Container from './components/Container'
@@ -6,18 +5,9 @@ import Header from './components/Header'
 import Main from './components/Main'
 import Sidebar from './components/Sidebar'
 import { useEffect } from 'react'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-
-  const[artistas, setArtistas] = useState([])
-
-  useEffect(() => {
-    fetch('http://localhost:3000/artistas')
-    .then( res => res.json())
-    .then( res => setArtistas(res))
-    .catch( err => console.log(err))
-    .finally(() => console.log("A")) 
-  }, [])
 
   return (
     <>
@@ -28,18 +18,7 @@ function App() {
           <CardSidebar/>
           <CardSidebar/>
         </Sidebar>
-        <Main>
-          <h1>PvZ</h1>
-          {
-            artistas
-            .filter(artista => artista.song === "pvz")
-            .map( artista => (
-              <div className='bg-red-700 w-28 h-28 flex flex-col justify-around items-center'>
-                <p>{artista.name}</p>
-              </div>
-            ))
-          }
-        </Main>
+        <Outlet/>
       </Container>
     </>
   )
